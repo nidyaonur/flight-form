69e6ae17917e0cb97041386b06af36b8045bacd5
Onur Aydin <nidya.onur@gmail.com>
2020-08-29 09:36:17 +0300

    css fixes, dummy api added and tested

--- flight/src/components/Flightform.js
+++ flight/src/components/Flightform.js
@@ -23,26 +23,32 @@ class FlightForm extends Component {
     this.onChangeDate = this.onChangeDate.bind(this);
   }
   onChange(e) {
-    console.log(e.target.name);
-    console.log(e.target.value);
     this.setState({ [e.target.name]: e.target.value });
+    console.log(this.state);
   }
   onCheck(e) {
     console.log(e);
     this.setState({ [e.target.name]: e.target.value });
   }
   onChangeDate(dates) {
-    //    this.setState({ startDate: e[0], endDate: e[1] });
+    if (this.state.flightType === "twoway")
+      this.setState({ startDate: dates[0], endDate: dates[1] });
+    else this.setState({ startDate: dates[0] });
     //const [start, end] = dates;
-    console.log(dates);
   }
   onSubmit(e) {
     e.preventDefault();
     const post = {
-      title: this.state.title,
-      body: this.state.body,
+      from: this.state.from,
+      to: this.state.to,
+      startDate: this.state.startDate,
+      endDate: this.state.endDate,
+      adults: this.state.adults,
+      children: this.state.children,
+      babies: this.state.babies,
+      flightType: this.state.flightType,
     };
-    fetch("https://jsonplaceholder.typicode.com/posts", {
+    fetch("https://postman-echo.com/post", {
       method: "POST",
       headers: { "content-type": "application/json" },
       body: JSON.stringify(post),
@@ -51,9 +57,6 @@ class FlightForm extends Component {
       .then((data) => console.log(data));
   }
   componentDidMount() {
-    fetch("https://jsonplaceholder.typicode.com/posts")
-      .then((res) => res.json())
-      .then((data) => this.setState({ posts: data }));
     this.setState({ startDate: new Date() });
   }
 
@@ -71,6 +74,7 @@ class FlightForm extends Component {
                   label="Outbound flight + return flight"
                   name="flightType"
                   value="twoway"
+                  checked
                 />
                 <Form.Check
                   onChange={this.onChange}
@@ -83,27 +87,33 @@ class FlightForm extends Component {
               </Form.Group>
             </fieldset>
           </Form.Row>
-          <Form.Row>
-            <Form.Group as={Col} controlId="formGridFrom">
+          <Form.Row className="justify-content-md-center">
+            <Form.Group as={Col} xs lg="2">
               <Form.Label>From</Form.Label>
               <Form.Control
+                name="from"
                 onChange={this.onChange}
                 as="select"
-                defaultValue="Choose..."
+                defaultValue="DusselDorf"
               >
-                <option>Choose...</option>
-                <option>...</option>
+                <option>DusselDorf</option>
+                <option>Antalya</option>
               </Form.Control>
             </Form.Group>
-            <Form.Group as={Col} controlId="formGridTo">
+            <Form.Group as={Col} xs lg="2">
               <Form.Label>To</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
+              <Form.Control
+                as="select"
+                name="to"
+                onChange={this.onChange}
+                defaultValue="Antalya"
+              >
+                <option>Antalya</option>
+                <option>DusselDorf</option>
               </Form.Control>
             </Form.Group>
 
-            <Form.Group as={Col} controlId="formGridTo">
+            <Form.Group as={Col} xs lg="2">
               <Form.Label>Date</Form.Label>
               <br />
               {this.state.flightType === "twoway" ? (
@@ -115,32 +125,61 @@ class FlightForm extends Component {
           </Form.Row>
 
           <Form.Row>
-            <Form.Group as={Col} controlId="formGridState">
+            <Form.Group as={Col}>
               <Form.Label>Adults 12+</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
+              <Form.Control
+                name="adults"
+                onChange={this.onChange}
+                as="select"
+                defaultValue="1"
+              >
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4</option>
+                <option>5</option>
+                <option>6</option>
               </Form.Control>
             </Form.Group>
-            <Form.Group as={Col} controlId="formGridState">
+            <Form.Group as={Col}>
               <Form.Label>Children 2-12</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
+              <Form.Control
+                name="children"
+                onChange={this.onChange}
+                as="select"
+                defaultValue="0"
+              >
+                <option>0</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4th</option>
               </Form.Control>
             </Form.Group>
-            <Form.Group as={Col} controlId="formGridState">
+            <Form.Group as={Col}>
               <Form.Label>Babies 0-2</Form.Label>
-              <Form.Control as="select" defaultValue="Choose...">
-                <option>Choose...</option>
-                <option>...</option>
+              <Form.Control
+                name="babies"
+                onChange={this.onChange}
+                as="select"
+                defaultValue="0"
+              >
+                <option>0</option>
+                <option>1</option>
+                <option>2</option>
+                <option>3</option>
+                <option>4th</option>
+                <option>5</option>
+                <option>6th</option>
               </Form.Control>
+            </Form.Group>
+            <Form.Group as={Col}>
+              <br />
+              <Button variant="primary" type="submit">
+                Search
+              </Button>
             </Form.Group>
           </Form.Row>
-
-          <Button variant="primary" type="submit">
-            Submit
-          </Button>
         </Form>
       </div>
     );
